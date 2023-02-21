dfec634961b85b29b1f7793af09e96afad94d90a
mohammadAlfian07 <moh.alfian.saidnaum@gmail.com>
2023-02-21 07:08:29 +0700

    zczc

--- src/main/java/com/sds/toms/viewmodel/TquestListVM.java
+++ src/main/java/com/sds/toms/viewmodel/TquestListVM.java
@@ -75,36 +75,8 @@ public class TquestListVM {
 
 					});
 
-					Button btnEdit = new Button();
-					btnEdit.setClass("btn btn-sm btn-success");
-					btnEdit.setIconSclass("z-icon-edit");
-					btnEdit.setStyle("border-radius:200px; margin:3px");
-					btnEdit.setTooltiptext("Ubah");
-					btnEdit.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
-
-						@Override
-						public void onEvent(Event event) throws Exception {
-						}
-
-					});
-
-					Button btnDelete = new Button();
-					btnDelete.setClass("btn btn-sm btn-danger");
-					btnDelete.setIconSclass("z-icon-trash");
-					btnDelete.setStyle("border-radius:200px; margin:3px");
-					btnDelete.setTooltiptext("Hapus");
-					btnDelete.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
-
-						@Override
-						public void onEvent(Event event) throws Exception {
-						}
-
-					});
-
 					Div div = new Div();
 					div.appendChild(btnDetail);
-					div.appendChild(btnEdit);
-					div.appendChild(btnDelete);
 					row.getChildren().add(div);
 
 				}
--- src/main/java/com/sds/toms/viewmodel/dosen/DraftSoalListVm.java
+++ src/main/java/com/sds/toms/viewmodel/dosen/DraftSoalListVm.java
@@ -1,21 +1,46 @@
 package com.sds.toms.viewmodel.dosen;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Checkbox;
+import org.zkoss.zul.Div;
 import org.zkoss.zul.Grid;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Row;
+import org.zkoss.zul.RowRenderer;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sds.toms.handler.RespHandler;
 import com.sds.toms.model.Muser;
+import com.sds.toms.model.Vquestcategory;
+import com.sds.toms.pojo.BanksoalReq;
+import com.sds.toms.pojo.ObjectResp;
+import com.sds.toms.util.AppUtil;
+import com.sds.utils.config.ConfigUtil;
 
 public class DraftSoalListVm {
 
 	private org.zkoss.zk.ui.Session zkSession = Sessions.getCurrent();
 	private Muser oUser;
-	
+
+	private Map<Long, BanksoalReq> map = new HashMap<>();
 	private Integer totalrecord;
 
 	@Wire
@@ -25,6 +50,114 @@ public class DraftSoalListVm {
 	public void afterCompose(@ContextParam(ContextType.VIEW) Component view) {
 		Selectors.wireComponents(view, this, false);
 		oUser = (Muser) zkSession.getAttribute("oUser");
+		doReset();
+		
+		if (grid != null) {
+			grid.setRowRenderer(new RowRenderer<BanksoalReq>() {
+
+				@Override
+				public void render(Row row, BanksoalReq data, int index) throws Exception {
+					row.getChildren().add(new Label(String.valueOf(index + 1)));
+
+					Checkbox check = new Checkbox();
+					check.setAttribute("obj", data);
+					check.addEventListener(Events.ON_CHECK, new EventListener<Event>() {
+						@Override
+						public void onEvent(Event event) throws Exception {
+							Checkbox checked = (Checkbox) event.getTarget();
+							if (checked.isChecked()) {
+								map.put(data.getId(), data);
+							} else {
+								map.remove(data.getId());
+							}
+						}
+					});
+					if (map.get(data.getId()) != null)
+						check.setChecked(true);
+					row.getChildren().add(check);
+					row.getChildren().add(new Label(data.getQuesttext()));
+					Button btnDetail = new Button();
+					btnDetail.setClass("btn btn-sm btn-info");
+					btnDetail.setIconSclass("z-icon-eye");
+					btnDetail.setStyle("border-radius:200px; margin:3px");
+					btnDetail.setTooltiptext("Detail");
+					btnDetail.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
+
+						@Override
+						public void onEvent(Event event) throws Exception {
+						}
+
+					});
+
+					Button btnEdit = new Button();
+					btnEdit.setClass("btn btn-sm btn-success");
+					btnEdit.setIconSclass("z-icon-edit");
+					btnEdit.setStyle("border-radius:200px; margin:3px");
+					btnEdit.setTooltiptext("Ubah");
+					btnEdit.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
+
+						@Override
+						public void onEvent(Event event) throws Exception {
+						}
+
+					});
+
+					Button btnDelete = new Button();
+					btnDelete.setClass("btn btn-sm btn-danger");
+					btnDelete.setIconSclass("z-icon-trash");
+					btnDelete.setStyle("border-radius:200px; margin:3px");
+					btnDelete.setTooltiptext("Hapus");
+					btnDelete.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
+
+						@Override
+						public void onEvent(Event event) throws Exception {
+						}
+
+					});
+
+					Div div = new Div();
+					div.appendChild(btnDetail);
+					div.appendChild(btnEdit);
+					div.appendChild(btnDelete);
+					row.getChildren().add(div);
+				}
+			});
+		}
 	}
-	
+
+	@NotifyChange("*")
+	public void doSearch() {
+		try {
+			ObjectResp Resp = null;
+			List<BanksoalReq> objList = new ArrayList<>();
+
+			String url = ConfigUtil.getConfig().getUrl_base() + ConfigUtil.getConfig().getEndpoint_tquest()
+					+ "/draft/dosen/" + oUser.getUserid();
+			Resp = RespHandler.responObj(url, null, AppUtil.METHOD_GET, oUser);
+
+			if (Resp.getCode() == 200) {
+				ObjectMapper mapper = new ObjectMapper();
+				objList = mapper.convertValue(Resp.getData(), new TypeReference<List<BanksoalReq>>() {
+				});
+
+				grid.setModel(new ListModelList<>(objList));
+				totalrecord = objList.size();
+			} else {
+				System.out.println("null");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void doReset() {
+		try {
+			totalrecord = 0;
+			doSearch();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
