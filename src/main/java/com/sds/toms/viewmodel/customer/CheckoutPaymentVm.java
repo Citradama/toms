12067dafb015a9a0b86b4174d32e8bc673c8b302
mohammadAlfian07 <moh.alfian.saidnaum@gmail.com>
2023-03-24 19:54:01 +0700

    gjhvhj

--- src/main/java/com/sds/toms/viewmodel/customer/CheckoutPaymentVm.java
+++ src/main/java/com/sds/toms/viewmodel/customer/CheckoutPaymentVm.java
@@ -1,6 +1,8 @@
 package com.sds.toms.viewmodel.customer;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
@@ -8,10 +10,16 @@ import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.HtmlNativeComponent;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.Selectors;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Div;
+import org.zkoss.zul.Image;
 import org.zkoss.zul.Label;
+import org.zkoss.zul.Separator;
+import org.zkoss.zul.Window;
 
 import com.sds.toms.model.Mpaymethod;
 import com.sds.toms.util.AppData;
@@ -20,6 +28,8 @@ public class CheckoutPaymentVm {
 
 	@Wire
 	private Div divMethod;
+	@Wire
+	private Window winPaymethod;
 	
 	@AfterCompose
 	public void afterCompose(@ContextParam(ContextType.VIEW) Component view) {
@@ -33,9 +43,27 @@ public class CheckoutPaymentVm {
 		try {
 			List<Mpaymethod> objList = AppData.getMpaymethod();
 			if(objList.size() > 0) {
+
+				Div divCard = new Div();
+				divCard.setClass("card");
+				
+				Div divCardBody = new Div();
+				divCardBody.setClass("card-body");
+				
 				for(Mpaymethod data : objList) {
 					Div divRow = new Div();
 					divRow.setClass("row");
+					divRow.setStyle("cursor:hand;cursor:pointer");
+					divRow.addEventListener(Events.ON_CLICK, new EventListener<Event>() {
+
+						@Override
+						public void onEvent(Event event) throws Exception {
+							Map<String, Object> map = new HashMap<>();
+							map.put("paymethod", data);
+							Event closeEvent = new Event("onClose", winPaymethod, map);
+							Events.postEvent(closeEvent);
+						}
+					});
 					
 					Div divCol = new Div();
 					divCol.setClass("col-10");
@@ -46,13 +74,23 @@ public class CheckoutPaymentVm {
 					divCol = new Div();
 					divCol.setClass("col-2");
 					divCol.setAlign("right");
-					label = new Label(">");
-					divCol.appendChild(label);
+					
+					Image image = new Image();
+					image.setSrc("/images/arrow.png");
+					image.setWidth("20px");
+					divCol.appendChild(image);
+					
 					divRow.appendChild(divCol);
+					
+					Separator separator = new Separator();
+					divRow.appendChild(separator);
 					
 					HtmlNativeComponent hr = new HtmlNativeComponent("hr");
 					divRow.appendChild(hr);
-					divMethod.appendChild(divRow);
+					
+					divCardBody.appendChild(divRow);
+					divCard.appendChild(divCardBody);
+					divMethod.appendChild(divCard);
 				}
 			}
 			
