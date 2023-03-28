1a4141ccafcfbbb00e79e528a94d613f87cc9b42
mohammadAlfian07 <moh.alfian.saidnaum@gmail.com>
2023-03-28 18:44:34 +0700

    ffdf

--- src/main/java/com/sds/toms/viewmodel/dosen/QuestPendingApproveVm.java
+++ src/main/java/com/sds/toms/viewmodel/dosen/QuestPendingApproveVm.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
@@ -32,19 +31,15 @@ import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Row;
 import org.zkoss.zul.RowRenderer;
-import org.zkoss.zul.Tab;
-import org.zkoss.zul.Tabs;
 import org.zkoss.zul.Window;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sds.toms.handler.RespHandler;
-import com.sds.toms.model.Mcategory;
 import com.sds.toms.model.Muser;
 import com.sds.toms.pojo.BanksoalReq;
 import com.sds.toms.pojo.ObjectResp;
 import com.sds.toms.pojo.QuestApprovalReq;
-import com.sds.toms.util.AppData;
 import com.sds.toms.util.AppUtil;
 import com.sds.utils.config.ConfigUtil;
 
@@ -59,19 +54,15 @@ public class QuestPendingApproveVm {
 
 	private Integer totalrecord;
 	private String reason;
-	private Long categoryid;
 
 	@Wire
 	private Grid grid;
-	@Wire
-	private Tabs tabs;
 
 	@AfterCompose
 	public void afterCompose(@ContextParam(ContextType.VIEW) Component view) {
 		Selectors.wireComponents(view, this, false);
 		oUser = (Muser) zkSession.getAttribute("oUser");
 
-		doRendertabs();
 		doReset();
 		if (grid != null) {
 			grid.setRowRenderer(new RowRenderer<BanksoalReq>() {
@@ -128,38 +119,14 @@ public class QuestPendingApproveVm {
 		}
 	}
 
-	public void doRendertabs() {
-		try {
-			List<Mcategory> categories = AppData.getMcategory();
-			for (Mcategory data : categories) {
-				if (categoryid == null)
-					categoryid = data.getId();
-				Tab tab = new Tab(data.getCategory());
-				tab.addEventListener(Events.ON_SELECT, new EventListener<Event>() {
-
-					@Override
-					public void onEvent(Event event) throws Exception {
-						doSearch(data.getId());
-						categoryid = data.getId();
-
-						BindUtils.postNotifyChange(null, null, QuestPendingApproveVm.class, "categoryid");
-					}
-				});
-				tab.setParent(tabs);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	@NotifyChange("*")
-	public void doSearch(Long categoryid) {
+	public void doSearch() {
 		try {
 			ObjectResp Resp = null;
 			List<BanksoalReq> objList = new ArrayList<>();
 
 			String url = ConfigUtil.getConfig().getUrl_base() + ConfigUtil.getConfig().getEndpoint_tquest()
-					+ "/category/" + categoryid;
+					+ "/waitapproval";
 			Resp = RespHandler.responObj(url, null, AppUtil.METHOD_GET, oUser);
 
 			if (Resp.getCode() == 200) {
@@ -185,7 +152,7 @@ public class QuestPendingApproveVm {
 		map = new HashMap<>();
 		objReq = new QuestApprovalReq();
 		reason = "";
-		doSearch(categoryid);
+		doSearch();
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
